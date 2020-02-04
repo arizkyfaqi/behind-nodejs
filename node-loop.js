cf0f8f95d4e38d828cef6649131cb0dd62fad546
arfa <arfa@example.com>
2020-02-04 12:14:06 +0700

    initial commit seccond

--- node-loop.js
+++ node-loop.js
@@ -1,15 +1,41 @@
-const fs = require('fs')
+const fs = require("fs");
+const crypto = require("crypto");
 
-setTimeout(() => console.log("Timer 1 finished"), 0)
-setImmediate(() => console.log("Immediate 1 finished"))
+const start = Date.now();
+//Thread pool size
+process.env.UV_THREADPOOL_SIZE = 2;
 
-fs.readFile('text-file.txt', () => {
-    console.log('I/O finished')
-    console.log('-------------')
+//out of event loop
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
 
-    setTimeout(() => console.log("Timer 2 finished"), 0)
-    setTimeout(() => console.log("Timer 3 finished"), 3000)
-    setImmediate(() => console.log("Immediate 2 finished"))
-})
+fs.readFile("text-file.txt", () => {
+  console.log("I/O finished");
+  console.log("-------------");
+  //   in event loop
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  //   get executed once per tick
+  setImmediate(() => console.log("Immediate 2 finished"));
 
-console.log("Hello from the top-level code")
+  //   get executed immediately
+  process.nextTick(() => console.log("Process.nextTick"));
+
+  //offloaded to the thread pool
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+
+  //   Sync version
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+});
+
+console.log("Hello from the top-level code");
